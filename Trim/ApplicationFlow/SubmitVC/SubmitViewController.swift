ee219cb7bfe5dcb45793314e7242d6e9474b08c2
Shane Nelson <shanenelson@shanes-mbp.attlocal.net>
2020-06-22 17:16:10 -0400

    95% done, need to observe the firebase requests using RX

--- Trim/ApplicationFlow/SubmitVC/SubmitViewController.swift
+++ Trim/ApplicationFlow/SubmitVC/SubmitViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MessageUI
 
-class SubmitViewController: UIViewController {
+class SubmitViewController: UIViewController, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +17,7 @@ class SubmitViewController: UIViewController {
     }
     
     @IBAction func submitButtonTap(_ sender: Any) {
+        //showMailComposeViewController()
         launchMailComposeViewController()
     }
     
@@ -40,19 +41,53 @@ class SubmitViewController: UIViewController {
     func launchMailComposeViewController() {
         if MFMailComposeViewController.canSendMail() {
             let controller = SMMailComposeViewController(recepients: ["swiftyshane@gmail.com"], subject: "Shane Nelson Coding Challenge", messageBody: "Body", messageBodyIsHTML: false)
+            controller.mailComposeDelegate = self
             self.present(controller, animated: true, completion: nil)
         }
     }
     
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title , message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
+}
+
+//MARK: - MailComposeDelegate
+extension SubmitViewController: MFMailComposeViewControllerDelegate {
+   
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        
+        switch result {
+        case .cancelled:
+            break
+        case .saved:
+            break
+        case .sent:
+            showAlert(title: "Success", message: "Your email / Submission has succeffully been sent, good luck candidate.")
+        case .failed:
+            showAlert(title: "Error", message: "Unable to send email.")
+        @unknown default:
+            print("Unknown default occurred.")
+        }
+        
+        if let error = error {
+            print(error.localizedDescription)
+        }
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
 
 //MARK: - Render
 extension SubmitViewController {
-
+    func render(_ state: SubmitViewControllerState) {
+        switch state {
+        case .submitError(_):
+            break
+        case .submitSuccess:
+            break
+        }
+    }
 }
 
-
-extension SubmitViewController: MFMailComposeViewControllerDelegate {
-    
-}

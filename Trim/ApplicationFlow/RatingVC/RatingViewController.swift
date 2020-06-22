59c43e5c9df82acb3a4ed5631266fc3d4ca1a957
Shane Nelson <shanenelson@shanes-mbp.attlocal.net>
2020-06-22 12:48:28 -0400

    added submitViewController

--- Trim/ApplicationFlow/RatingVC/RatingViewController.swift
+++ Trim/ApplicationFlow/RatingVC/RatingViewController.swift
@@ -9,8 +9,26 @@
 import UIKit
 
 class RatingViewController: UIViewController {
+    
+    var pointsRemaining: Int?
+    var textFieldTextAsIntArray: [Int]?
 
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var pointsRemainingTextField: UITextField!
+    
+    @IBOutlet weak var uikitTextField: UITextField!
+    @IBOutlet weak var modularDevelopmentTextField: UITextField!
+    @IBOutlet weak var memoryManagementTextField: UITextField!
+    @IBOutlet weak var testingTextField: UITextField!
+    @IBOutlet weak var coreDataTextField: UITextField!
+    @IBOutlet weak var debuggingTextField: UITextField!
+    @IBOutlet weak var problemSolvingTextField: UITextField!
+    @IBOutlet weak var SwiftUITextField: UITextField!
+    @IBOutlet weak var workingOnTeamTextField: UITextField!
+    @IBOutlet weak var selfMotivationTextField: UITextField!
+    @IBOutlet weak var communicationSkillsTextField: UITextField!
+    @IBOutlet weak var energyLevelTextField: UITextField!
+    @IBOutlet weak var intelligenceTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +37,57 @@ class RatingViewController: UIViewController {
     
     func setupUI() {
         backgroundView.layer.cornerRadius = 2
+        pointsRemainingTextField.isUserInteractionEnabled = false
     }
-
+    
+    @IBAction func saveButtonTap(_ sender: Any) {
+        navigateToSubmitViewController()
+    }
+    
+    func getAllTextFields(fromView view: UIView)-> [UITextField] {
+        return view.subviews.compactMap { (view) -> [UITextField]? in
+            if view is UITextField {
+                return [(view as! UITextField)]
+            } else {
+                return getAllTextFields(fromView: view)
+            }
+        }.flatMap({$0})
+    }
+    
+    func updatePointsRemaining() {
+        textFieldTextAsIntArray = [] // Reset the array each time.
+        pointsRemaining = 0
+        
+        let textFields = getAllTextFields(fromView: self.view)
+        
+        textFields.forEach({ textField in
+            if let text = textField.text, var textFieldArray = textFieldTextAsIntArray {
+                if let textAsNumber = Int(text) {
+                    textFieldArray.insert(textAsNumber, at: 0)
+                }
+            }
+        })
+        let sumOfTextFields = textFieldTextAsIntArray?.reduce(0, +)
+        pointsRemainingTextField.text = "\(String(describing: sumOfTextFields))"
+        //pointsRemaining = sumOfTextFields
+    }
+    
+    func navigateToSubmitViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        if let submitViewController = storyboard.instantiateViewController(withIdentifier: "SubmitViewController") as? SubmitViewController {
+            submitViewController.modalPresentationStyle = .fullScreen
+            self.present(submitViewController, animated: true, completion: nil)
+        } else {
+            preconditionFailure("Could not navigate to SubmitViewController")
+        }
+    }
+    
 }
 
 extension RatingViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        updatePointsRemaining()
         let nextTag = textField.tag + 1
         if let nextResponder = textField.superview?.viewWithTag(nextTag) {
             nextResponder.becomeFirstResponder()
---
+++ Trim/ApplicationFlow/SubmitVC/SubmitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SubmitViewController.swift
+//  Trim
+//
+//  Created by Shane Nelson on 6/22/20.
+//  Copyright © 2020 Shane Nelson. All rights reserved.
+//
+
+import UIKit
+
+class SubmitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}

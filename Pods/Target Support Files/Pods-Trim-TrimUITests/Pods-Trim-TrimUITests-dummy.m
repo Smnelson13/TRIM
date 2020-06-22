60c9c633b8f80eda2b2bc90ba6f6c2f8e8393780
Shane Nelson <shanenelson@shanes-mbp.attlocal.net>
2020-06-21 22:51:37 -0400

    added firebase for login and storage

---
+++ Pods/Target Support Files/Pods-Trim-TrimUITests/Pods-Trim-TrimUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trim_TrimUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Trim_TrimUITests
+@end
---
+++ Trim/ApplicationFlow/CreateAccount/CreateAccountViewControllerState.swift
@@ -0,0 +1,9 @@
+//
+//  CreateAccountViewControllerState.swift
+//  Trim
+//
+//  Created by Shane Nelson on 6/21/20.
+//  Copyright © 2020 Shane Nelson. All rights reserved.
+//
+
+import Foundation
